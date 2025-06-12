f87dabbe97ad4ea98e749464fab79ddd4fe8d4d7
Dane Weber <dane.weber@excella.com>
2025-06-12 15:42:52 -0400

    fix: filename now matters

--- render.sh
+++ render.sh
@@ -3,11 +3,14 @@ set -euo pipefail
 
 applications=$(find ./chart/values -type f \( -name "*.yaml" -o -name "*.yml" \) -print | sort)
 value_files=$(echo "$applications" | sed 's/^/-f /')
-helm template argo-cd-app-of-apps ./chart/charts/parent-chart -f ./chart/values.yaml $value_files --output-dir rendered-manifests
+helm template argo-cd-app-of-apps ./chart/charts/parent-chart \
+  -f ./chart/values.yaml \
+  $value_files \
+  --output-dir rendered-manifests
 
 for application_yaml in $applications; do
   echo "Rendering application: $application_yaml"
-  appname=$(basename $(dirname "$application_yaml"))
+  appname=$(basename "$application_yaml" | sed 's/\.[^.]*$//')
   clusters=$(yq eval ".$appname.clusters[].name" "$application_yaml")
   for clustername in $clusters; do
     helm template "${appname}.${clustername}" \
